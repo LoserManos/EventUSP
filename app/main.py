153189e4595a0bf51cfae174878f436e19d20135
nattan447 <nattanferreira58@gmail.com>
2026-05-24 17:57:37 -0300

    criei rotas de auth e lógica do JWT para autenticação

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from app.database import get_session, create_db_and_tables
+from app.routes import auth,events,users
 app = FastAPI()
+
+app.include_router(auth.router) ## inclui as rotas do auth no arquivo principal
 
 @app.on_event("startup")
 def on_startup():
-    ## cria as tabelas do banco de dados(se não existirem)
-    create_db_and_tables()
+    create_db_and_tables() ##cria as tabelas do banco de dados(se não existirem)
 
 @app.get("/")
 def root():
---
+++ app/routes/auth.py
@@ -0,0 +1,31 @@
+from app.security import generate_hash_password, verify_password, create_access_token, get_actual_user
+from fastapi import FastAPI, Depends, HTTPException, status,APIRouter
+from sqlmodel import Session, select
+from app.database import get_session
+from app.models import User
+from app.schemas import LoginRequest, TokenResponse,SingupRequest,SingupResponse
+
+# criando o router
+router = APIRouter(tags=["Autenticação"])
+
+@router.post("/auth/login",status_code=status.HTTP_200_OK,response_model=TokenResponse)
+def login(login_data: LoginRequest,session = Depends(get_session)):
+    query = select(User).where(login_data.email==login_data.email)
+    user = session.exec(query).first()
+    if not user or verify_password(login_data.password,user.password):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="E-mail ou senha incorretos")
+    token = create_access_token({"sub":user.id})
+    return {"acces_token":token,"detail":"bearer","id":user.id}
+ 
+@router.post("/auth/singup",status_code=status.HTTP_201_CREATED,response_model=SingupResponse)
+def singup(singup_data:SingupRequest,session = Depends(get_session)):
+    query = select(User).where(singup_data.email==singup_data.email)
+    user = session.exec(query).first()
+    if user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Este e-mail já está cadastrado no sistema.")
+    criptd_passord = generate_hash_password(singup_data.password)
+    new_user = User(name=singup_data.name,email=singup_data.email,password = criptd_passord,bio = singup_data.bio)
+    session.add(new_user)
+    session.commit()
+    session.refresh(new_user)
+    return new_user
